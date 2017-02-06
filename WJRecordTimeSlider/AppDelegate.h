6a7a31f19465b09f065b98b2dab31622ad65e7ce
wynton <973316202@qq.com>
2017-02-06 09:20:35 +0800

    Create Project

---
+++ WJRecordTimeSlider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WJRecordTimeSlider
+//
+//  Created by Wynton on 2017/2/6.
+//  Copyright © 2017年 wynton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WJRecordTimeSlider/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WJRecordTimeSlider
+//
+//  Created by Wynton on 2017/2/6.
+//  Copyright © 2017年 wynton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
